46cf76e1772ee03da7bd899545c3166982449875
Ariel Soares <ariel.sfranco@gmail.com>
2024-02-27 09:56:48 -0300

    OrderRepository added and configured

--- src/main/java/com/ariel/course/resources/OrderResource.java
+++ src/main/java/com/ariel/course/resources/OrderResource.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ariel.course.entities.User;
-import com.ariel.course.services.UserService;
+import com.ariel.course.entities.Order;
+import com.ariel.course.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResources {
+@RequestMapping(value = "/orders")
+public class OrderResource {
 
 	@Autowired
-	private UserService service;
+	private OrderService service;
 	
 	@GetMapping
-	private ResponseEntity<List<User>> findAll(){
-		List<User> list = service.findAll();
+	private ResponseEntity<List<Order>> findAll(){
+		List<Order> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		User obj = service.findById(id);
+	public ResponseEntity<Order> findById(@PathVariable Long id){
+		Order obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
