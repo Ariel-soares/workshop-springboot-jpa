282e3321576faf9da65e06fc8c3af2c96d42ac20
Ariel Soares <ariel.sfranco@gmail.com>
2024-03-05 19:50:14 -0300

    Update exception added

--- src/main/java/com/ariel/course/services/UserService.java
+++ src/main/java/com/ariel/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.ariel.course.repositories.UserRepository;
 import com.ariel.course.services.exception.DatabaseException;
 import com.ariel.course.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
